9fe22a98e48597b87663cac96ae39ee58639070b
Rikikudo <trunghieu99tt@gmail.com>
2020-07-03 12:51:46 +0700

    Updates

---
+++ precache-manifest.d05482ca8e1c93ff9a43bc49bd691db3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac629b1c933766f4f91a2585c853dbc6",
+    "url": "/Portfolio-Page/index.html"
+  },
+  {
+    "revision": "b4abb5e4a65334f5e6c3",
+    "url": "/Portfolio-Page/static/css/main.6d059b3b.chunk.css"
+  },
+  {
+    "revision": "efafde699a65360a6c4a",
+    "url": "/Portfolio-Page/static/js/2.c743e518.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio-Page/static/js/2.c743e518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4abb5e4a65334f5e6c3",
+    "url": "/Portfolio-Page/static/js/main.01ad1950.chunk.js"
+  },
+  {
+    "revision": "fd0d3a819e0b1e2bbe29",
+    "url": "/Portfolio-Page/static/js/runtime-main.5641c713.js"
+  },
+  {
+    "revision": "e238a942aaa7cbcde8496c3ab629458f",
+    "url": "/Portfolio-Page/static/media/avante.e238a942.PNG"
+  },
+  {
+    "revision": "74b8c3c5d47fc48ab06bc983a75fd332",
+    "url": "/Portfolio-Page/static/media/avatar.74b8c3c5.jpg"
+  },
+  {
+    "revision": "5e005f87df2b2e7df481fa9215f89736",
+    "url": "/Portfolio-Page/static/media/chrismas-min.5e005f87.PNG"
+  },
+  {
+    "revision": "43cf6051d6225b2459f90feae57f6cbe",
+    "url": "/Portfolio-Page/static/media/clothing-shop-min.43cf6051.PNG"
+  },
+  {
+    "revision": "2c7c31062d378b9360c21c38c3e7d4dc",
+    "url": "/Portfolio-Page/static/media/cosmeticshop-min.2c7c3106.PNG"
+  },
+  {
+    "revision": "bad6fa053d3f5d4b5268cc134c0f22b4",
+    "url": "/Portfolio-Page/static/media/goibacsi.bad6fa05.PNG"
+  },
+  {
+    "revision": "0b77d168b54563a4febdd37532c9445f",
+    "url": "/Portfolio-Page/static/media/img4-min.0b77d168.png"
+  },
+  {
+    "revision": "abd36a9dea479139bbc9604d907023d7",
+    "url": "/Portfolio-Page/static/media/img5.abd36a9d.jpg"
+  },
+  {
+    "revision": "6c63a1aa951ff21d4bd258810e0a8359",
+    "url": "/Portfolio-Page/static/media/memory-card-min.6c63a1aa.PNG"
+  },
+  {
+    "revision": "73378638a26a80cf2be7f35b1f254390",
+    "url": "/Portfolio-Page/static/media/messenger.73378638.PNG"
+  },
+  {
+    "revision": "bd9550df4e9e05637ab2be089604358e",
+    "url": "/Portfolio-Page/static/media/music-player-min.bd9550df.PNG"
+  },
+  {
+    "revision": "54c1714b0cbb5daf0a2ccce69ce6df82",
+    "url": "/Portfolio-Page/static/media/nhathuoc-min.54c1714b.PNG"
+  },
+  {
+    "revision": "a2fca4e3cb8c983aea873fa7e7354226",
+    "url": "/Portfolio-Page/static/media/omnifood-min.a2fca4e3.PNG"
+  },
+  {
+    "revision": "427f803f3e21822f1d268c98c62f418e",
+    "url": "/Portfolio-Page/static/media/tigren-logo.427f803f.png"
+  },
+  {
+    "revision": "68e1267b56517cc5aad624745f371cc7",
+    "url": "/Portfolio-Page/static/media/todo-min.68e1267b.PNG"
+  },
+  {
+    "revision": "c7c10ad1b010b3160a38db973f20a90e",
+    "url": "/Portfolio-Page/static/media/vgasoft-min.c7c10ad1.PNG"
+  },
+  {
+    "revision": "6205c6636c02e22694925147624be9c1",
+    "url": "/Portfolio-Page/static/media/videoblog-min.6205c663.PNG"
+  }
+]);
